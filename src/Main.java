07325b4ffafab04c8f93395f06f2eaeba4af8ff3
andreisophie <andreisophie@yahoo.com>
2022-12-22 19:25:28 +0200

    Finished homework

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
@@ -11,6 +13,10 @@ public class Main {
      *           3 for Floyd-Warshall
      */
     public static void main(String[] args) throws IOException{
+        String outPath = new String("times.txt");
+        File outFile = new File(outPath);
+        outFile.createNewFile();
+        FileWriter outWriter = new FileWriter(outFile);
         for (int i = 1; i <= NR_TESTS; i++) {
             RWOperations.createFiles(i);
 
@@ -18,11 +24,14 @@ public class Main {
 
             Instant start = Instant.now();
             myAlgorithm.run();
+            
             Instant end = Instant.now();
             Duration duration = Duration.between(start, end);
             System.out.println("Execution time test " + i + ": " + duration.toSeconds() + "s " + duration.toMillis() % 1000 + "ms " + duration.toNanos() % 1000000 + "ns");
+            outWriter.write(String.format("%6.3e", (double)duration.toNanos() / 1000000000) + "\n");
 
             RWOperations.writeOutputs(myAlgorithm);
         }
+        outWriter.close();
     }
 }
